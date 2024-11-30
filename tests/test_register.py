45ae10f92d94769d6faca1e32eab8547b1b41303
John Makarov <johnmakarovqa@gmail.com>
2024-11-30 23:52:35 +0300

    add slow mark

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,13 +1,26 @@
-from core.settings import fake_name_for_registration
+from faker import Faker
+import pytest
 
+
+fake = Faker()
 
 def test_register_as_tutor(header, register):
     header.visit()
     header.click_on_registration_button()
     register.header_should_contain_text("Регистрация")
-    register.fill_nick(fake_name_for_registration)
+    register.fill_nick(fake.user_name())
     register.fill_password("sdjflsfdjlksdjflksdjf")
     register.fill_confirm_password("sdjflsfdjlksdjflksdjf")
     register.click_on_become_a_teacher_button()
     register.click_on_registration_button()
     header.create_listing_button_should_be_visible()
+
+@pytest.mark.slow
+def test_register_as_student(header, register):
+    header.visit()
+    header.click_on_registration_button()
+    register.header_should_contain_text("Регистрация")
+    register.fill_nick(fake.user_name())
+    register.fill_password("sdjflsfdjlksdjflksdjf")
+    register.fill_confirm_password("sdjflsfdjlksdjflksdjf")
+    register.click_on_registration_button()
