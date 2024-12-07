f7e787cb552542f4d03944ba373999dd687c4137
Roman Burlaka <101947109+RomanBurlaka78@users.noreply.github.com>
2024-12-07 11:24:26 +0100

    US_02.002.001 | Guest-Header > Registretion(button) > Visibility (#82)

--- components/login.py
+++ components/login.py
@@ -11,3 +11,9 @@ class Login:
     def check_url_login_page(self, expected_part: str):
         current_url = self.page.url
         assert expected_part in current_url, f"Ожидалось, что '{expected_part}' будет частью '{current_url}'"
+
+    @allure.step("Проверяем наличие кнопки 'Регистрация'")
+    def check_title_of_registration(self):
+        title = self.page.get_by_title("Регистрация")
+        assert "Регистрация",  title
+
