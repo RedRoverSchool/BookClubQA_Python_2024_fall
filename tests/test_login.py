a712fd966894015f3e57c1d869c1a592d7e78613
KatyaMy <20FamGam20@gmail.com>
2024-12-09 09:49:27 -0500

    install plugins for ci

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,7 @@
 def test_login_as_tutor_btn_create_listing(header, login):
     login.open_login_page()
-    header.click_on_login_button
+    header.click_on_login_button()
     login.enter_username("test_auth_login")
     login.enter_password("test_auth_pass")
     login.click_login_button()
-    header.create_listing_button_should_be_visible
+    header.create_listing_button_should_be_visible()
