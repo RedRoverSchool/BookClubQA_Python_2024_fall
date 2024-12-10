5b413b633e4757c153702b90f0459040dff812e6
yuliyafil22 <yulverkz@gmail.com>
2024-12-10 13:35:53 +0500

    TC_32.001.001.001 | Student > My teachers> Viewing My Teachers List > Navigate to the "My Teachers" Page      .Изменила - в каждом тесте новая регистрация студента-как и просил Виктор. А также добавила третий тест на проверку отображения списка репетиторов либо сообщения об отсутствии репетиторов. В итоге в каждом тесте-одна проверка.

--- tests/test_btn_my_teachers.py
+++ tests/test_btn_my_teachers.py
@@ -1,27 +1,48 @@
 from playwright.sync_api import Page
 from components.login import Login
+from core.settings import fake
+from components.register import Register
 from components.my_teachers import MyTeachersPage
 
 #TC_32.001.001.001 | Student > My teachers> Viewing My Teachers List > Navigate to the "My Teachers" Page
 
-def test_my_teachers_btn_exists(header, page: Page):
+def test_my_teachers_btn_exists(header, register, page: Page):
     """Проверка наличия кнопки 'Мои репетиторы'."""
     header.visit()
-    header.click_on_login_button()
-    login_page = Login(page)
-    login_page.full_login("Yuliya", "sdjflsfdjlksdjflksdjf")   #пока так логинюсь
+    header.click_on_registration_button()
+    register.header_should_contain_text("Регистрация")
+    register.fill_nick(fake.user_name())
+    register.fill_password("sdjflsfdjlksdjflksdjf")
+    register.fill_confirm_password("sdjflsfdjlksdjflksdjf")
+    register.click_on_registration_button()
     my_teachers_button = MyTeachersPage(page)
     my_teachers_button.check_my_teachers_btn_exists()
 
 
-def test_my_teachers_btn_click(header, page: Page):
+def test_my_teachers_btn_click(header, register,page: Page):
     """Проверка клика на кнопку 'Мои репетиторы'."""
     header.visit()
-    header.click_on_login_button()
-    login_page = Login(page)
-    login_page.full_login("Yuliya", "sdjflsfdjlksdjflksdjf")
+    header.click_on_registration_button()
+    register.header_should_contain_text("Регистрация")
+    register.fill_nick(fake.user_name())
+    register.fill_password("sdjflsfdjlksdjflksdjf")
+    register.fill_confirm_password("sdjflsfdjlksdjflksdjf")
+    register.click_on_registration_button()
     my_teachers_button = MyTeachersPage(page)
     my_teachers_button.click_my_teachers_btn()
     my_teachers_button.verify_page_my_teachers_opened()
+
+
+def test_teachers_list(header, register, page: Page):
+    """Проверка что список репетиторов отображается или нет'."""
+    header.visit()
+    header.click_on_registration_button()
+    register.header_should_contain_text("Регистрация")
+    register.fill_nick(fake.user_name())
+    register.fill_password("sdjflsfdjlksdjflksdjf")
+    register.fill_confirm_password("sdjflsfdjlksdjflksdjf")
+    register.click_on_registration_button()
+    my_teachers_button = MyTeachersPage(page)
+    my_teachers_button.click_my_teachers_btn()
     my_teachers_button.check_teachers_list()
 
