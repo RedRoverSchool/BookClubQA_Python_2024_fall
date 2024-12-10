8475979c93d7d06207b9450a97f7ecaa9ef6b8fc
Liliana <lilianakoric@gmail.com>
2024-12-09 23:10:11 -0800

    TC_03.001.005_check_invalid_username added: test_login.py, data folder, @pytest.fixture def browser_context():

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,12 +6,12 @@ from Data.data import invalid_login, valid_password
 
 
 def test_login_as_tutor_btn_create_listing(header, login):
-    login.open_login_page()
-    header.click_on_login_button
+    header.visit()
+    header.click_on_login_button()
     login.enter_username("test_auth_login")
     login.enter_password("test_auth_pass")
     login.click_login_button()
-    header.create_listing_button_should_be_visible
+    
 
 
 @allure.title("TC_03.001.005")
