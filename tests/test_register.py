c41a3e6bc54eb4c5b98fceb7171d097da0aaed73
yuliyafil22 <yulverkz@gmail.com>
2024-12-11 19:40:39 +0500

    TC_35.001.001.001

--- tests/test_register.py
+++ tests/test_register.py
@@ -25,3 +25,15 @@ def test_register_as_student(header, register):
     register.fill_password("sdjflsfdjlksdjflksdjf")
     register.fill_confirm_password("sdjflsfdjlksdjflksdjf")
     register.click_on_registration_button()
+
+
+#TC_35.001.001.001 | Student >Become a teacher > Navigate to the "Стать репетитором" page
+def test_become_a_teacher_from_student_page(header, login, homepage, register):
+    """Проверка перехода на страницу регистрации как репетитор из профиля студента."""
+    header.visit()
+    header.click_on_login_button()
+    login.full_login("student_test", "]<c%ZTHH8EZ3L–+")
+    header.visit()
+    homepage.check_2_find_tutor_btns()
+    homepage.click_become_tutor_btn()
+    register.verify_registration_page_opened()
