577dc4461c432b79ebfa871ed76941b3d185e43c
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2024-12-12 22:44:12 +0500

    Fix test_profile_btn_redirection functiion name

--- tests/test_user_profile.py
+++ tests/test_user_profile.py
@@ -4,7 +4,7 @@ from core.settings import site_pages_urls
 
 
 @pytest.mark.parametrize('data', site_pages_urls, ids=[u["name"] for u in site_pages_urls])
-def test_profile_btn_visibility(user_profile, header, login, data, page: Page):
+def test_profile_btn_visibility(user_profile, login, data, page: Page):
     """Проверка видимости кнопки 'Профиль'
     TC_11.002.01.001 | [Teacher ] Header > "Профиль" button > Visibility check"""
     login.open_login_page()
@@ -14,7 +14,7 @@ def test_profile_btn_visibility(user_profile, header, login, data, page: Page):
     user_profile.visibility_profile_btn_check()
 
 @pytest.mark.parametrize('data', site_pages_urls, ids=[u["name"] for u in site_pages_urls])
-def test_profile_btn_hover(user_profile, header, login, data, page: Page):
+def test_profile_btn_hover(user_profile, login, data, page: Page):
     """Проверка смены цвета кнопки 'Профиль' при наведении на нее курсора
     TC_11.002.01.002 | [Teacher ] Header > "Профиль" button > Hover support check"""
     login.open_login_page()
@@ -24,7 +24,7 @@ def test_profile_btn_hover(user_profile, header, login, data, page: Page):
     user_profile.hover_profile_btn_color_check()
 
 @pytest.mark.parametrize('data', site_pages_urls, ids=[u["name"] for u in site_pages_urls])
-def test_profile_btn_hover(user_profile, header, login, data, page: Page):
+def test_profile_btn_redirection(user_profile, login, data, page: Page):
     """Проверка перенаправления на страницу профиля пользователя после нажатия кнопки 'Профиль'
     TC_11.002.01.003 | [Teacher ] Header > "Профиль" button > Redirection check"""
     login.open_login_page()
