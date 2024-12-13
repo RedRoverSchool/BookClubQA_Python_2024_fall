2717fe1b26a239dc8faaec5bf77942c8f85b01ad
tgusachenko <tnka516@gmail.com>
2024-12-13 16:46:18 -0600

    add placeholders test

--- components/register.py
+++ components/register.py
@@ -38,9 +38,57 @@ class Register:
     def verify_registration_page_opened(self):
         registration_title = self.page.locator("h1").inner_text()
         assert (
-            registration_title == "Регистрация"
+                registration_title == "Регистрация"
         ), f"Ожидался заголовок 'Регистрация', но найдено: {registration_title}"
 
     @allure.step("Создаем случайный пароль")
     def generate_valid_password(self):
         self.password = data.generate_valid_password()
+
+    @allure.step("Проверяем, что плейсхолдер виден в поле 'username'")
+    def verify_username_placeholder_visibility(self):
+        is_placeholder_visible = self.page.locator("#id_username").evaluate("""
+                (el) => {
+                    const placeholderStyle = window.getComputedStyle(el, '::placeholder');
+                    return placeholderStyle && placeholderStyle.visibility !== 'hidden' && placeholderStyle.opacity > 0;
+                }
+            """)
+        assert is_placeholder_visible, "The placeholder for 'username' is not visible"
+
+    @allure.step("Проверяем текст плейсхолдера в поле 'username'")
+    def check_username_placeholder_text(self):
+        assert (
+                self.page.locator("#id_username").get_attribute("placeholder") == "Придумайте ник"
+        ), "Incorrect placeholder text in the 'username' field"
+
+    @allure.step("Проверяем, что плейсхолдер виден в поле 'password1'")
+    def verify_password1_placeholder_visibility(self):
+        is_placeholder_visible = self.page.locator("#id_password1").evaluate("""
+                    (el) => {
+                        const placeholderStyle = window.getComputedStyle(el, '::placeholder');
+                        return placeholderStyle && placeholderStyle.visibility !== 'hidden' && placeholderStyle.opacity > 0;
+                    }
+                """)
+        assert is_placeholder_visible, "The placeholder for 'password1' is not visible"
+
+    @allure.step("Проверяем текст плейсхолдера в поле 'password1'")
+    def check_password1_placeholder_text(self):
+        assert (
+                self.page.locator("#id_password1").get_attribute("placeholder") == "Придумайте пароль"
+        ), "Incorrect placeholder text in the 'password1' field"
+
+    @allure.step("Проверяем, что плейсхолдер виден в поле 'password2'")
+    def verify_password2_placeholder_visibility(self):
+        is_placeholder_visible = self.page.locator("#id_password2").evaluate("""
+                        (el) => {
+                            const placeholderStyle = window.getComputedStyle(el, '::placeholder');
+                            return placeholderStyle && placeholderStyle.visibility !== 'hidden' && placeholderStyle.opacity > 0;
+                        }
+                    """)
+        assert is_placeholder_visible, "The placeholder for 'password2' is not visible"
+
+    @allure.step("Проверяем текст плейсхолдера в поле 'password2'")
+    def check_password2_placeholder_text(self):
+        assert (
+                self.page.locator("#id_password2").get_attribute("placeholder") == "Подтвердите пароль"
+        ), "Incorrect placeholder text in the 'password2' field"
