0f96f21299b7a8880c5f0c899101dd2e65959ba3
OxanaSum <oksana.sumina@gmail.com>
2024-12-17 00:52:52 +0200

    TC_05.001.006.003  | Find a Teacher > Filter Search (Teacher) > Filter Teacher [min_price] | Entring the min_price from the keyboard

--- components/find_tutor.py
+++ components/find_tutor.py
@@ -1,11 +1,17 @@
 import allure
-from playwright.sync_api import Page
-from core.settings import list_url
+from playwright.sync_api import Page, expect
+from core.settings import list_url, tutors_list_url
 
 
 class FindTutor:
     def __init__(self, page: Page):
         self.page = page
+
+
+    @allure.step("Открываем Найти Репетитора на главной странице")
+    def open_find_tutor_page(self):
+        self.page.goto(tutors_list_url)
+
 
     @allure.step("Проверяем наличие кнопки 'Регистрация'")
     def check_title_of_registration(self):
@@ -41,3 +47,30 @@ class FindTutor:
     def check_message_of_registration(self, expected_message):
         message = self.page.locator("//div[@role='alert']").text_content()
         assert message.strip() == expected_message, f"Expected text is \'{expected_message}\', but received \'{message.strip()}\'"
+
+
+    @allure.step("Нажимаем на кнопку Фильтровать")
+    def click_filter_button(self):
+        self.page.get_by_text("Фильтровать").click()
+
+
+    @allure.step("Вводим значение минимальной цены")
+    def enter_min_price(self, min_price: float):
+        price_field = self.page.locator("#minPrice")
+        price_field.fill(str(min_price))
+
+
+    @allure.step("Проверяем, что цены за занятие репетиторов больше или равны заданного значения")
+    def check_prices_over_min_price(self, min_price: float) -> object:
+        # Проверяем, есть ли репетиторы на странице
+        teachers_list_price = self.page.locator(".card-text")
+
+        if teachers_list_price.count() > 0:
+            # Если репетиторы есть, то проверяем, что их стоимость занятия >= установленной минимальной цены
+            for i in range(teachers_list_price.count()):
+                actual_price = float(teachers_list_price.nth(i).text_content().strip().split(" ")[1])
+                assert actual_price >= min_price
+        else:
+            # Если список репетиторов пустой, то проверяем сообщение
+            text = self.page.locator("//div[@class='alert alert-info']").text_content()
+            assert "Нет результатов", text
