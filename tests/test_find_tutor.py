eff4b3ad65575f8f11eec4cd949c2d0b25d149d0
Zarina Umbetova <zaya110589@gmail.com>
2024-12-17 23:15:47 -0500

    TC_05.001.006.004 | Find a Teacher > Filter Search (Teacher) > Filter Teacher [min_price is generated randomly] #287 (#288)

--- tests/test_find_tutor.py
+++ tests/test_find_tutor.py
@@ -1,4 +1,7 @@
 import allure
+from faker import Faker
+
+fake = Faker()
 
 @allure.title("TC_05.001.006.003")
 @allure.link("https://github.com/RedRoverSchool/BookClubQA_Python_2024_fall/issues/249")
@@ -20,3 +23,9 @@ def test_redirect_to_tutors_page_with_0_min_price(find_tutor):
     find_tutor.check_filter_btn_is_visible()
     find_tutor.click_filter_button()
     find_tutor.check_prices_over_min_price(0)
+
+def test_verify_tutors_after_entered_min_price_by_random(find_tutor):
+    find_tutor.open_find_tutor_page()
+    find_tutor.add_random_min_price(fake)
+    find_tutor.click_filter_button()
+    find_tutor.check_prices_over_min_price(fake.random_int(min=1, max=1000))
