72452bfc13710ef627600cf81cf64abb146290ec
mishasro72 <mishasro72@gmail.com>
2024-12-19 15:28:11 -0500

    TC_13.005.002. Verify the teacher is able go to the page Мое объявление

--- tests/test_teacher_profile.py
+++ tests/test_teacher_profile.py
@@ -1,3 +1,16 @@
+import allure
+
+
+@allure.title("TC_13.005.002")
+def test_teacher_able_get_announcement_page(header, login, announcement):
+    login.open_login_page()
+    login.enter_username("kick01@mail.ru")
+    login.enter_password("123QW654")
+    login.click_login_button()
+    announcement.click_my_announcement_button()
+    announcement.verify_my_announcements_page_endpoint()
+
+
 
 
 
