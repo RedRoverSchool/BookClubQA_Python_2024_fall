002e852e19e30d6895200c0cd00d5defa6f077af
KatyaMy <20FamGam20@gmail.com>
2024-12-20 20:28:16 -0500

    added API structure

---
+++ models/user_model.py
@@ -0,0 +1,15 @@
+# Модели (Pydantic) — для валидации данных.
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+
+class RegisterRequest(BaseModel):
+    first_name: str = Field(..., title="Ваше имя", max_length=30, min_length=1)
+    email: EmailStr = Field(..., title="Email", max_length=254, min_length=1)
+    password: str = Field(..., title="Пароль", max_length=128, min_length=6)
+
+    is_tutor: Optional[bool] = Field(True, title="Я репетитор")
+    is_premium: Optional[bool] = Field(False, title="Is premium")
+    is_standart: Optional[bool] = Field(False, title="Is standart")
+    is_writer: Optional[bool] = Field(False, title="Is writer")
+    end_subscription: Optional[str] = Field(None, title="End subscription", nullable=True)
