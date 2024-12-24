cf13996c272107aa9a6c245bbd0a3e84e6bd2501
VolhaDev <o2015tara@gmai.com>
2024-12-23 19:32:49 -0800

    TC_15.001.002 | Header - Teacher > "Вoйти" - button is available when user isn't logged in#352

--- tests/test_login_button_availability.py
+++ tests/test_login_button_availability.py
@@ -1,11 +1,7 @@
-import allure
-import pytest
-
-
 def test_login_button_present_and_enabled_when_not_logged_in(homepage):
-       """
-       Verify that the "Войти" button is present and enabled in the header when a user is not logged in.
-       """
-       homepage.visit()
-       # Use the new method from homepage.py
-       homepage.check_login_button()
+    """
+    Verify that the "Войти" button is present and enabled in the header when a user is not logged in.
+    """
+    homepage.visit()
+    # Use the new method from homepage.py
+    homepage.check_login_button()
