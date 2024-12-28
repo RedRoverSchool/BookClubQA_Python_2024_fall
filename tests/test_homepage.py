2fc73b17558cee3f80f4ab0651d6b7108586901c
usergalina1 <50974306+usergalina1@users.noreply.github.com>
2024-12-28 00:04:59 -0800

    fix locator for the button after UI was changed (#380)

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -51,7 +51,6 @@ def test_more_btn_redirects_telegram_page_for_tutors(homepage, telegram_page):
     telegram_page.check_telegram_channel_should_have_title_for_tutors()
 
 
-@pytest.mark.skip(reason="не прошёл CI после изменений 26.12.2024")
 # TC_00.002.002.001 | Main page (body) - All users > Become a Teacher >
 # Verify button "Стать репетитором" is visible below "Добро пожаловать в "Мыслеплав"!" heading #77
 def test_first_btn_become_a_tutor_is_visible(homepage):
