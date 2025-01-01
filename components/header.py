a389c95235d29e88bd8136c0c521fa1bcbb8e105
Roman Burlaka <burlakarv@gmail.com>
2025-01-01 20:33:06 +0100

    RF ->skipped | AT_02.003.001.001 Changed locator

--- components/header.py
+++ components/header.py
@@ -96,7 +96,7 @@ class Header:
 
     @allure.step("Проверяем видимость кнопки 'Найти репетитора'")
     def find_a_tutor_button_should_be_visible(self):
-        button = self.page.locator("//a[@href ='/listings/list/']/i")
+        button = self.page.locator("//*[@id='navbarNav']//a[@href ='/listings/list/']")
         assert button.is_visible()
 
     @allure.step("Кликаем на кнопку 'Статистика'")
