24b639b2200c9d6b8175b6febcd92265b2427860
Zarina <zaya110589@gmail.com>
2025-01-11 22:41:58 -0500

    fixed

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,11 +5,6 @@ from Data.data import invalid_login, valid_password
 
 error_messages = "Пожалуйста, введите правильные email и пароль. Оба поля могут быть чувствительны к регистру."
 
-
-def test_login_as_tutor(login, header):
-    login.login_as_tutor(header)
-
-
 @pytest.mark.skip(reason="не прошёл CI после изменений 26.12.2024")
 def test_login_as_tutor_btn_create_listing(header, login):
     header.visit()
