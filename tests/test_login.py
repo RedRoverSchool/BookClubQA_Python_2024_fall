de1d0c8d7d43610486aa43bba008687ec634426b
KatyaMy <20FamGam20@gmail.com>
2025-01-17 14:59:07 -0500

    Replacing login and password

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,24 +1,23 @@
 import re
 
 import allure
-import pytest
+
 from playwright.sync_api import expect
 
-from Data.data import invalid_login, valid_password
+from Data.data import EMAIL_TUTOR_KM, PASSWORD_TUTOR_KM, invalid_login, valid_password
 
 error_messages = "Пожалуйста, введите правильные email и пароль. Оба поля могут быть чувствительны к регистру."
 
 
-@pytest.mark.skip(reason="не прошёл CI после изменений 26.12.2024")
 def test_login_as_tutor_btn_create_listing(header, login):
     header.visit()
     header.click_login_button()
-    login.enter_username("test_auth_login")
-    login.enter_password("test_auth_pass")
+    login.enter_username(EMAIL_TUTOR_KM)
+    login.enter_password(PASSWORD_TUTOR_KM)
     login.click_submit_button()
 
 
-@allure.title("TC_03.001.005")
+@allure.label('test_id',"TC_03.001.005")
 @allure.link("https://github.com/RedRoverSchool/BookClubQA_Python_2024_fall/issues/17")
 def test_check_enter_invalid_username(header, login):
     header.visit()
